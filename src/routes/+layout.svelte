f6ab91a532ac2a7b900c24352ab527886e8512bd
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-19 16:01:50 +1100

    Add store to allow player to hide nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import { showNav } from './showNav.store';
+	$showNav = true;
 </script>
 
-<nav>
-	<h1><a href="/">Desert Bus for Hope Quiz Builders</a></h1>
-	<h3><a href="this-or-that">This or That</a></h3>
-	<h3><a href="only-connect">Only Connect</a></h3>
-</nav>
+{#if $showNav}
+	<nav>
+		<h1><a href="/">Desert Bus for Hope Quiz Builders</a></h1>
+		<h3><a href="this-or-that">This or That</a></h3>
+		<h3><a href="only-connect">Only Connect</a></h3>
+	</nav>
+{/if}
 
 <slot />
