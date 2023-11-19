a2ca4bb668d918177b49cee0d9f2ba08a3b68b1b
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-19 14:26:12 +1100

    Fix "auto close on paste" error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <nav>
-	<h1></h1><a href="/">Desert Bus for Hope Quiz Builders</a></h1>
+	<h1><a href="/">Desert Bus for Hope Quiz Builders</a></h1>
 	<h3><a href="this-or-that">This or That</a></h3>
 	<h3><a href="only-connect">Only Connect</a></h3>
 </nav>
