858fdb359e2f232fbe43b5be3b71151d15c9aa14
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-19 17:16:01 +1100

    Style navbar with tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,21 @@
 </script>
 
 {#if $showNav}
-	<nav>
-		<h1><a href="/">Desert Bus for Hope Quiz Builders</a></h1>
-		<h3><a href="this-or-that">This or That</a></h3>
-		<h3><a href="only-connect">Only Connect</a></h3>
+	<nav class="flex h-14 w-screen items-center justify-between border-b border-slate-900/10">
+		<home class="inline-block w-1/4">
+			<a href="/">
+				<img src="logo-trans-100.png" alt="Desert Bus for Hope" class="inline-block h-12 w-12" />
+				<h1 class="inline-block">Quiz Builders</h1>
+			</a>
+		</home>
+		<ul class="inline-block flex w-1/2 items-center justify-evenly">
+			<li class="inline-block"><a href="this-or-that">This or That</a></li>
+			<li class="inline-block"><a href="only-connect">Only Connect</a></li>
+			<li class="inline-block"><a href="millionaire">Who Wants to be a Millionaire</a></li>
+		</ul>
+		<help class="inline-block w-1/6 text-right">
+			<button class="h-12 w-12 rounded-full text-center ring-1 ring-slate-900/10">?</button>
+		</help>
 	</nav>
 {/if}
 
