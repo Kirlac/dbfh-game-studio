77e517888ec7bd48928580551949501145360a11
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-19 16:05:03 +1100

    Clean up player
    Replace close shortcut with a double-press confirmation system.
    Add comments to document code.
    Import and use showNav store to hide nav bar when player is active.

--- src/routes/this-or-that/player.svelte
+++ src/routes/this-or-that/player.svelte
@@ -1,21 +1,93 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { showNav } from '../showNav.store';
 
+	// Declare json value object representing our game
 	export let value = '';
 
+	// Create event dispatcher for communicating with parent
 	const dispatch = createEventDispatcher();
 
-	function handleKeyPress(this: Document, ev: KeyboardEvent) {
-		if (ev.key === 'Escape' && ev.ctrlKey) {
-			// raise close player event
-			dispatch('close');
-			document.removeEventListener('keyup', handleKeyPress);
-		}
+	// Intermediary param for close confirmation to prevent accidental closure
+	// - Press once to show confirmation
+	// - Press again when confirmation is visible to actually close
+	let escapePressed = false;
+
+	// Helper function to show close confirmation for 1000ms
+	function showCloseConfirmation() {
+		escapePressed = true;
+		setTimeout(() => {
+			escapePressed = false;
+		}, 1000);
 	}
 
-	document.addEventListener('keyup', handleKeyPress);
+	// Keyup listener for handling key presses
+	function handleKeyPress(this: Document, ev: KeyboardEvent) {
+		// TODO: Convert if block to switch/case for handling full keyboard interaction
+		// Escape should confirm and close the player to return to the editor
+		if (ev.key === 'Escape') {
+			if (!escapePressed) {
+				showCloseConfirmation();
+			} else {
+				// raise close player event
+				dispatch('close');
+				$showNav = true;
+				document.removeEventListener('keyup', handleKeyPress);
+			}
+		}
+		// ??? should select "this" value
+		// ??? should select "that" value
+		// ??? should select "both" value
+		// ??? should select "neither" value
+		// etc.
+	}
+
+	// Mounting logic
+	onMount(() => {
+		$showNav = false;
+		// Bind keyup event listener
+		document.addEventListener('keyup', handleKeyPress);
+		// Flash close confirmation message on load so user knows how to exit
+		showCloseConfirmation();
+	});
 </script>
 
 <h2>Player</h2>
-<p>Press <code>ctrl</code> + <code>escape</code> to return to editor</p>
 <pre>{value}</pre>
+
+{#if escapePressed}
+	<overlay out:fade>
+		<h2>Press <code>ESC</code> key to return to editor</h2>
+	</overlay>
+{/if}
+
+<style>
+	overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		/* background-color: #000a; */
+		display: flex;
+		align-items: flex-end;
+		justify-content: center;
+	}
+
+	overlay h2 {
+		color: #eee;
+		background-color: #000a;
+		font-size: 32px;
+		text-align: center;
+		display: inline-block;
+		position: relative;
+		padding: 8px;
+		border-radius: 4px;
+	}
+
+	code,
+	pre {
+		font-family: 'Courier New', Courier, monospace;
+	}
+</style>
