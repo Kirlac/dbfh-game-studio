eb5b439699bd3ab10e7324e4808a06b0fc0f39f9
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-19 18:04:42 +1100

    Clean up error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,21 +8,40 @@
 	}
 </script>
 
-<h1>There was an error</h1>
+<header class="text-center">
+	<h1 class="m-4 inline-block text-4xl">There was an error</h1>
+</header>
 
-{#if $page.status === 404}
-	<h2>Page does not exist</h2>
-	<h3>Please check the URL and try again</h3>
-	<h4>If you're sure the page should exist, please file a bug report</h4>
-{/if}
+<section class="text-center">
+	{#if $page.status === 404}
+		<h2 class="m-4 inline-block text-2xl">Page does not exist</h2>
+		<br />
+		<h3 class="m-4 inline-block text-xl">Please check the URL and try again</h3>
+		<br />
+		<h4 class="m-4 inline-block text-lg">
+			If you're sure the page should exist, please file a bug report
+		</h4>
+		<br />
+	{/if}
 
-<button on:click={toggleDetails}>{detailsVisible ? 'Hide' : 'Show'} error details</button>
+	<button
+		class="m-4 rounded-md p-2 ring-1 ring-slate-900/10 hover:bg-slate-900/10"
+		on:click={toggleDetails}
+	>
+		{detailsVisible ? 'Hide' : 'Show'} error details
+	</button>
+</section>
 
 {#if detailsVisible}
-	<p>Status code: {$page.status}</p>
-	{#if $page?.error?.message}
-		<p>Message: {$page?.error?.message}</p>
-	{/if}
-	<p>Details:</p>
-	<pre>{JSON.stringify($page, null, 2)}</pre>
+	<section class="mx-auto w-1/2 rounded-md bg-slate-900/10 p-4 ring-1 ring-slate-900/10">
+		<p>Status code: {$page.status}</p>
+		<br />
+		{#if $page?.error?.message}
+			<p>Message: {$page?.error?.message}</p>
+			<br />
+		{/if}
+		<p>Details:</p>
+		<br />
+		<pre>{JSON.stringify($page, null, 2)}</pre>
+	</section>
 {/if}
