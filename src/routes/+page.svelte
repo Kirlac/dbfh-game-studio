b7ffaf06cbd04d52de5392235d30b748fbbd21e2
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-19 18:07:19 +1100

    Implement home/landing page placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<p>Use the above navigation to select a quiz type to create or play.</p>
+<header class="text-center">
+	<img src="logo-trans-200.png" alt="Desert Bus for Hope" class="inline-block" />
+	<br />
+	<h1 class="m-4 inline-block text-4xl">The Desert Bus <s>2023</s> 2024 Quiz Builder</h1>
+</header>
+
+<section class="text-center">
+	<h2 class="m-4 inline-block text-2xl">Play a Quiz</h2>
+	<br />
+	<p>Paste your gameplay code in the box below then click start.</p>
+	<br />
+	<textarea
+		class="h-32 w-3/4 resize-none rounded-md p-2 ring-1 ring-slate-900/10"
+		placeholder="Paste your gameplay code here then click start"
+	></textarea>
+	<br />
+	<button class="rounded-md p-2 ring-1 ring-slate-900/10 hover:bg-slate-900/10"> Start </button>
+</section>
+
+<section class="text-center">
+	<h2 class="m-4 inline-block text-2xl">Create a Quiz</h2>
+	<p>
+		Use the navigation bar at the top of the page to select the quiz type you want to to create.
+		Instructions for each individual quiz will be provided on their respective pages.
+	</p>
+</section>
