0a48c42c8affc29570890f1cebf7acb125112561
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-21 04:30:34 +1100

    Remove navigation bar
    Links were added to the home page making this redundant.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import '../app.css';
-	import { showNav } from './showNav.store';
-	$showNav = true;
 
 	onMount(async () => {
 		// Import boxicons in onMount because it needs to reference 'window'
@@ -10,42 +8,16 @@
 	});
 </script>
 
-{#if $showNav}
-	<nav class="flex h-14 w-screen items-center justify-between border-b border-slate-900/10">
-		<home>
-			<a href="/">
-				<img
-					src="logo-2024-trans-100.png"
-					alt="Desert Bus for Hope"
-					class="inline-block h-12 w-12"
-				/>
-			</a>
-		</home>
-		<ul class="flex w-1/2 items-center justify-evenly">
-			<li class="inline-block">
-				<a class="rounded-md px-4 py-2 hover:bg-slate-900/10" href="this-or-that">This or That</a>
-			</li>
-			<li class="inline-block">
-				<a class="rounded-md px-4 py-2 hover:bg-slate-900/10" href="only-connect">Only Connect</a>
-			</li>
-			<li class="inline-block">
-				<a class="rounded-md px-4 py-2 hover:bg-slate-900/10" href="millionaire"
-					>Who Wants to be a Millionaire</a
-				>
-			</li>
-			<li class="inline-block">
-				<a class="rounded-md px-4 py-2 hover:bg-slate-900/10" href="guess-the-thing"
-					>Guess the Thing</a
-				>
-			</li>
-		</ul>
-		<help class="flex items-center text-right">
-			<button class="mx-2 flex h-12 w-12 items-center justify-center rounded-full">
-				<box-icon type="solid" name="help-circle" class="h-10 w-10 opacity-25 hover:opacity-50"
-				></box-icon>
-			</button>
-		</help>
-	</nav>
-{/if}
+<a
+	class="absolute left-0 top-0 mx-2 flex h-12 w-12 items-center justify-center rounded-full"
+	href="/"
+>
+	<box-icon type="solid" name="home" class="h-10 w-10 opacity-25 hover:opacity-50"></box-icon>
+</a>
+
+<button class="absolute right-0 top-0 mx-2 flex h-12 w-12 items-center justify-center rounded-full">
+	<box-icon type="solid" name="help-circle" class="h-10 w-10 opacity-25 hover:opacity-50"
+	></box-icon>
+</button>
 
 <slot />
