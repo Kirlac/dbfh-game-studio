e5a49de977e108a8e261b33448a202f1dc0fcf9b
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-21 04:34:04 +1100

    Clean up home page
    Replace game code text area with GameCode component.
    
    Include page links from nav bar in the "create" section.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import GameCode from '$lib/GameCode.svelte';
+
+	function decodeGameCode(e: CustomEvent<any>): void {
+		throw new Error('Function not implemented.');
+	}
+</script>
+
 <header class="text-center">
 	<img src="logo-2024-trans-200.png" alt="Desert Bus for Hope" class="inline-block" />
 	<br />
@@ -5,22 +13,40 @@
 </header>
 
 <section class="text-center">
-	<h2 class="m-4 inline-block text-2xl">Play a Quiz</h2>
-	<br />
-	<p>Paste your gameplay code in the box below then click start.</p>
-	<br />
-	<textarea
-		class="m-2 h-32 w-3/4 resize-none rounded-md p-2 ring-1 ring-slate-900/10"
-		placeholder="Paste your gameplay code here then click start"
-	></textarea>
-	<br />
-	<button class="m-2 rounded-md p-2 ring-1 ring-slate-900/10 hover:bg-slate-900/10"> Start </button>
+	<h2 class="m-4 inline-block text-2xl">Create a new game</h2>
+	<p>Click one of the below buttons to create a new game of that type.</p>
+	<ul class="m-2 flex items-center justify-center">
+		<li>
+			<a
+				class="m-2 inline-block rounded-md bg-stone-50 px-4 py-2 ring-1 ring-stone-900/20 hover:bg-stone-200"
+				href="this-or-that">This or That</a
+			>
+		</li>
+		<li>
+			<a
+				class="m-2 inline-block rounded-md bg-stone-50 px-4 py-2 ring-1 ring-stone-900/20 hover:bg-stone-200"
+				href="only-connect">Only Connect</a
+			>
+		</li>
+		<li>
+			<a
+				class="m-2 inline-block rounded-md bg-stone-50 px-4 py-2 ring-1 ring-stone-900/20 hover:bg-stone-200"
+				href="millionaire">Who Wants to be a Millionaire</a
+			>
+		</li>
+		<li>
+			<a
+				class="m-2 inline-block rounded-md bg-stone-50 px-4 py-2 ring-1 ring-stone-900/20 hover:bg-stone-200"
+				href="guess-the-thing">Guess the Thing</a
+			>
+		</li>
+	</ul>
 </section>
 
 <section class="text-center">
-	<h2 class="m-4 inline-block text-2xl">Create a Quiz</h2>
-	<p>
-		Use the navigation bar at the top of the page to select the quiz type you want to to create.
-		Instructions for each individual quiz will be provided on their respective pages.
-	</p>
+	<h2 class="m-4 inline-block text-2xl">Play or edit a game</h2>
+	<br />
+	<p>Paste your game code in the box below then click the play button.</p>
+	<br />
+	<GameCode value="" showEditButton showPlayButton on:change={decodeGameCode}></GameCode>
 </section>
