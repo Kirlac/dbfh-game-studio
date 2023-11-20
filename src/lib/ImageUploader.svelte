bc545f9edc252a6681a0514fa8196a20b0d480ba
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-21 04:31:43 +1100

    Move image uploader logic to its own component
    For better reusability.

---
+++ src/lib/ImageUploader.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { imageToBase64 } from './Base64Encoder';
+
+	let imageBase64: unknown;
+	async function loadImage(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+		let file = event?.currentTarget?.files?.[0];
+		if (file) {
+			imageBase64 = await imageToBase64(file);
+		}
+	}
+</script>
+
+<section>
+	<h3>Image to base64 converter test</h3>
+	<input type="file" on:change={loadImage} />
+	<pre class="whitespace-break-spaces break-words">
+			{imageBase64}
+		</pre>
+</section>
