78d151e8c443c6aa8cfbcb72930a16f858625b3e
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-21 05:16:02 +1100

    Clean up home and help icons on layout
    Use regular variants instead of solid.
    
    Specify lighter opaque colors instead of changing opacity.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,12 +12,11 @@
 	class="absolute left-0 top-0 mx-2 flex h-12 w-12 items-center justify-center rounded-full"
 	href="/"
 >
-	<box-icon type="solid" name="home" class="h-10 w-10 opacity-25 hover:opacity-50"></box-icon>
+	<box-icon name="home" class="h-10 w-10 fill-stone-400 hover:fill-stone-600"></box-icon>
 </a>
 
 <button class="absolute right-0 top-0 mx-2 flex h-12 w-12 items-center justify-center rounded-full">
-	<box-icon type="solid" name="help-circle" class="h-10 w-10 opacity-25 hover:opacity-50"
-	></box-icon>
+	<box-icon name="help-circle" class="h-10 w-10 fill-stone-400 hover:fill-stone-600"></box-icon>
 </button>
 
 <slot />
