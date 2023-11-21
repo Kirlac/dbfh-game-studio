d4aaa9174d87ce8c9b3d1d7542bac8718fc5a8ee
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-21 16:33:44 +1100

    Clean up layout button styles
    Add background hover states.
    
    Remove unnecessary flex rules.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,12 @@
 	import '../app.css';
 </script>
 
-<a
-	class="absolute left-0 top-0 mx-2 flex h-12 w-12 items-center justify-center rounded-full"
-	href="/"
->
-	<BoxIcon name="home" class="h-10 w-10 fill-stone-400 hover:fill-stone-600"></BoxIcon>
+<a class="absolute left-0 top-0 m-2 h-12 w-12 rounded-md hover:bg-stone-200" href="/">
+	<BoxIcon name="home" class="h-full w-full fill-stone-400 hover:fill-stone-600"></BoxIcon>
 </a>
 
-<button class="absolute right-0 top-0 mx-2 flex h-12 w-12 items-center justify-center rounded-full">
-	<BoxIcon name="help-circle" class="h-10 w-10 fill-stone-400 hover:fill-stone-600"></BoxIcon>
+<button class="absolute right-0 top-0 m-2 h-12 w-12 rounded-full hover:bg-stone-200">
+	<BoxIcon name="help-circle" class="h-full w-full fill-stone-400 hover:fill-stone-600"></BoxIcon>
 </button>
 
 <slot />
