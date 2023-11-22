75c6fbabbbfa3bfd77caab0efae9b4099cfecf1e
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-22 19:35:32 +1100

    Add basic placeholder Desert Bus theme
    To start giving the application some visual appeal.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				dbfh: {
+					bglight: '#426FBB',
+					bgmid: '#30518B',
+					bgdark: '#253B9D',
+					bgdarker: '#24528f',
+					accent: '#56B4B6',
+					headlight: '#3874CB',
+					headdark: '#F7CC58',
+					text: '#454545'
+				}
+			}
+		}
 	},
 	plugins: []
 };
