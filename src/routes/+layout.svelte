fd9ca2b7b80bbafe10d4d4df06111b0403485f4b
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-22 14:12:07 +1100

    Make home and help centre buttons fixed
    So they stay at the top of the viewport regardless of scroll distance.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,12 +18,12 @@
 </script>
 
 {#if !isPlayer}
-	<a class="absolute left-0 top-0 m-2 h-12 w-12 rounded-md hover:bg-stone-200" href="/">
+	<a class="fixed left-0 top-0 m-2 h-12 w-12 rounded-md hover:bg-stone-200" href="/">
 		<Icon name="bus" class="h-full w-full text-stone-400 hover:text-stone-600"></Icon>
 	</a>
 
 	<button
-		class="absolute right-0 top-0 m-2 h-12 w-12 rounded-full hover:bg-stone-200"
+		class="fixed right-0 top-0 m-2 h-12 w-12 rounded-full hover:bg-stone-200"
 		on:click={openHelpCentre}
 	>
 		<Icon name="circle-info" class="h-full w-full text-stone-400 hover:text-stone-600"></Icon>
