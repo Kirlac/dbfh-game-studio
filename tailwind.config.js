7863c925cdfa09f79b41e77750240e547d9e6bc8
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-22 23:37:54 +1100

    Configure desert bus shift theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {
-			colors: {
-				dbfh: {
-					bglight: '#426FBB',
-					bgmid: '#30518B',
-					bgdark: '#253B9D',
-					bgdarker: '#24528f',
-					accent: '#56B4B6',
-					headlight: '#3874CB',
-					headdark: '#F7CC58',
-					text: '#454545'
+		colors: {
+			slate: colors.slate,
+			red: colors.red,
+			green: colors.green,
+			theme: {
+				primary: {
+					light: 'rgb(var(--theme-primary-light) / <alpha-value>)',
+					dark: 'rgb(var(--theme-primary-dark) / <alpha-value>)'
+				},
+				secondary: {
+					light: 'rgb(var(--theme-secondary-light) / <alpha-value>)',
+					dark: 'rgb(var(--theme-secondary-dark) / <alpha-value>)'
+				},
+				accent: {
+					light: 'rgb(var(--theme-accent-light) / <alpha-value>)',
+					dark: 'rgb(var(--theme-accent-dark) / <alpha-value>)'
+				},
+				neutral: {
+					light: 'rgb(var(--theme-neutral-light) / <alpha-value>)',
+					dark: 'rgb(var(--theme-neutral-dark) / <alpha-value>)'
 				}
 			}
-		}
+		},
+		extend: {}
 	},
 	plugins: []
 };
