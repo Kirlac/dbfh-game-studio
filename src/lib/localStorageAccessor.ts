25c7f78e135bd267d6ae31d0a260cbcd4b86e91e
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-22 14:14:32 +1100

    Add helper lib for accessing local storage
    So we can save client-side configs etc.

---
+++ src/lib/localStorageAccessor.ts
@@ -0,0 +1,25 @@
+import { browser } from '$app/environment';
+
+export function getValue(key: string, initial: string) {
+	let value = initial;
+	if (browser) {
+		value = localStorage.getItem(key) ?? setValue(key, initial);
+	}
+	return value;
+}
+
+export function getParsedValue(key: string, initial: any) {
+	const saved = JSON.parse(getValue(key, JSON.stringify(initial)));
+	return { ...initial, ...saved };
+}
+
+export function setValue(key: string, value: string) {
+	if (browser) {
+		localStorage.setItem(key, value);
+	}
+	return value;
+}
+
+export function setStringifiedValue(key: string, value: any) {
+	return setValue(key, JSON.stringify(value));
+}
