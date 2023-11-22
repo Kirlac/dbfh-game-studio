61ba923143c2d521cc12e74922ea6f01206cb75d
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-23 04:46:13 +1100

    Clean up GameCode component
    Add handlers for discard and edit buttons.
    
    Update play button handler to go to play screen.
    
    Remove broken gameData store references.
    
    Fix style classes, specifically for margins around button icons/text.

--- src/lib/components/GameCode.svelte
+++ src/lib/components/GameCode.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { fade } from 'svelte/transition';
 	import Icon from './Icon.svelte';
+	import { base64ToString, stringToBase64 } from '$lib/utils/base64Encoder';
 
 	export let value: string;
-	export let showEditButton = false;
 	export let showPlayButton = false;
+	export let showEditButton = false;
+	export let showDiscardButton = false;
 	export let showCopyButton = false;
 	export let placeholderText = 'Paste your game code or drop your .dbfhg file here';
 
@@ -19,7 +22,33 @@
 	}
 
 	function playGame() {
-		let mode = 'play';
+		goto('/play');
+	}
+
+	let emptyGame = {};
+	let emptyGameJson = JSON.stringify(emptyGame);
+	let emptyGameBase64 = stringToBase64(emptyGameJson);
+
+	let gameDataJson = '{}';
+
+	$: try {
+		gameData = base64ToString(value);
+	} catch (error) {
+		console.log(error);
+		gameData = '{}';
+	}
+
+	$: gameData = JSON.parse(gameDataJson);
+
+	function editGame() {
+		if (gameData?.gameType) {
+			goto(`/${gameData.gameType}`);
+		}
+	}
+
+	function discardGame() {
+		//value = emptyGameBase64;
+		value = '';
 	}
 
 	function copyGameCode() {
@@ -40,52 +69,56 @@
 			}
 		);
 	}
-
-	function editGame(event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
-		throw new Error('Function not implemented.');
-	}
-
-	function tryParseCode(event: Event & { currentTarget: EventTarget & HTMLElement }) {
-		let code = event.currentTarget.innerText;
-		if (code) {
-			value = code;
-		}
-		console.log(value);
-	}
 </script>
 
 <section
 	class="relative inline-block h-64 w-3/4 rounded-md bg-slate-50 text-left ring-1 ring-slate-900/10 hover:bg-slate-200 focus:bg-slate-200"
 >
-	{#if $gameData && (showPlayButton || showEditButton || showDeleteButton || showCopyButton)}
+	{#if value && (showPlayButton || showEditButton || showDiscardButton || showCopyButton)}
 		<menu class="absolute right-0 top-0 flex justify-around">
 			{#if showPlayButton}
 				<li>
 					<button
-						class="text-theme-accent-dark m-2 flex items-center justify-center rounded-md bg-slate-100 p-2 ring-1 ring-slate-900/10 hover:bg-slate-200"
+						class="m-2 flex items-center justify-center rounded-md bg-slate-100 p-2 text-theme-accent-dark ring-1 ring-slate-900/10 hover:bg-slate-200"
 						on:click={playGame}
 					>
-						<Icon name="play" class="mx-2"></Icon> Play
+						<Icon name="play" class="text-theme-neutral-dark"></Icon><span class="ml-2">Play</span>
 					</button>
 				</li>
 			{/if}
 			{#if showEditButton}
 				<li>
 					<button
-						class="text-theme-accent-dark m-2 flex items-center justify-center rounded-md bg-slate-100 p-2 ring-1 ring-slate-900/10 hover:bg-slate-200"
+						class="m-2 flex items-center justify-center rounded-md bg-slate-100 p-2 text-theme-accent-dark ring-1 ring-slate-900/10 hover:bg-slate-200"
 						on:click={editGame}
 					>
-						<Icon name="pen-to-square" class="mx-2"></Icon> Edit
+						<Icon name="pen-to-square" class="text-theme-neutral-dark"></Icon><span class="ml-2"
+							>Edit</span
+						>
+					</button>
+				</li>
+			{/if}
+			{#if showDiscardButton}
+				<li>
+					<button
+						class="m-2 flex items-center justify-center rounded-md bg-slate-100 p-2 text-theme-accent-dark ring-1 ring-slate-900/10 hover:bg-slate-200"
+						on:click={discardGame}
+					>
+						<Icon name="trash-can" class="text-theme-neutral-dark"></Icon><span class="ml-2"
+							>Discard</span
+						>
 					</button>
 				</li>
 			{/if}
 			{#if showCopyButton}
 				<li>
 					<button
-						class="text-theme-accent-dark m-2 flex items-center justify-center rounded-md bg-slate-100 p-2 ring-1 ring-slate-900/10 hover:bg-slate-200"
+						class="m-2 flex items-center justify-center rounded-md bg-slate-100 p-2 text-theme-accent-dark ring-1 ring-slate-900/10 hover:bg-slate-200"
 						on:click={copyGameCode}
 					>
-						<Icon name="clipboard" class="mx-2"></Icon> Copy
+						<Icon name="clipboard" class="text-theme-neutral-dark"></Icon><span class="ml-2"
+							>Copy</span
+						>
 					</button>
 				</li>
 			{/if}
@@ -95,7 +128,7 @@
 	<pre
 		contenteditable="true"
 		bind:innerText={value}
-		class="h-full w-full overflow-scroll whitespace-break-spaces break-all p-2 {!$gameCode
+		class="h-full w-full overflow-scroll whitespace-break-spaces break-all p-2 {!value
 			? 'placeholder text-theme-neutral-dark/40'
 			: 'text-theme-neutral-dark'}"
 		data-placeholder-text={placeholderText}></pre>
