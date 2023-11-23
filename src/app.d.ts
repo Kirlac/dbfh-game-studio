3dd0297db289f9c03b98129d3873f720e5b15ccd
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-23 22:04:55 +1100

    Add layout for game pages
    Using PageData to pass details and sub-components into the template.
    
    Each page can specify its own title, default value, instructions, etc.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,16 @@
+import type { GameData } from '$lib/stores/gameData.store';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			title: string;
+			defaultGameData: GameData;
+			instructions: ConstructorOfATypedSvelteComponent;
+		}
 		// interface Platform {}
 	}
 }
--- src/routes/(games)/+layout.svelte
+++ src/routes/(games)/+layout.svelte
@@ -3,21 +3,23 @@
 	import type { GameData } from '$lib/stores/gameData.store';
 	import GameCode from '$lib/components/GameCode.svelte';
 	import Icon from '$lib/components/Icon.svelte';
+	import { page } from '$app/stores';
 
-	const defaultGameData: GameData = {
-		gameType: 'this-or-that',
-		titleCard: {},
-		questions: [],
-		endCard: {}
-	};
-
-	let gameData = defaultGameData;
+	let gameData: GameData = $page?.data?.defaultGameData;
 	$: gameDataJson = JSON.stringify(gameData);
 	$: gameDataBase64 = stringToBase64(gameDataJson);
 
 	async function loadExample() {
-		const exampleResponse = await fetch('this-or-that-example.json');
-		gameData = await exampleResponse.json();
+		const exampleResponse = await fetch(`${gameData.gameType}-example.dbfhg`);
+		let exampleGameCode = await exampleResponse.text();
+		let exampleGameData = JSON.parse(exampleGameCode);
+		gameData = exampleGameData;
+	}
+
+	let instructionsVisible = true;
+
+	function toggleInstructionsVisibility() {
+		instructionsVisible = !instructionsVisible;
 	}
 
 	let codeVisible = false;
@@ -29,7 +31,9 @@
 
 <header class="flex flex-col items-center justify-around text-center">
 	<img src="logo-2024-trans-200.png" alt="Desert Bus for Hope" class="inline-block w-[200px]" />
-	<h1 class="m-4 inline-block text-4xl text-theme-accent-light">This or That</h1>
+	<h1 class="m-4 inline-block text-4xl text-theme-accent-light">
+		{$page.data.title || 'The Desert Bus for Hope Game Studio'}
+	</h1>
 </header>
 
 <main class="text-center">
@@ -38,7 +42,6 @@
 			<li>
 				<button
 					class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
-					on:click={loadExample}
 				>
 					<Icon name="file" class="text-theme-neutral-dark"></Icon><span class="ml-2">New Game</span
 					>
@@ -57,10 +60,19 @@
 			<li>
 				<button
 					class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
-					on:click={toggleCodeVisibility}
 				>
 					<Icon name="play" class="text-theme-neutral-dark"></Icon><span class="ml-2"
 						>Play Game</span
+					>
+				</button>
+			</li>
+			<li>
+				<button
+					class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
+					on:click={toggleInstructionsVisibility}
+				>
+					<Icon name="book-open-reader" class="text-theme-neutral-dark"></Icon><span class="ml-2"
+						>{instructionsVisible ? 'Hide' : 'Show'} Instructions</span
 					>
 				</button>
 			</li>
@@ -76,12 +88,17 @@
 			</li>
 		</menu>
 
+		{#if instructionsVisible}
+			<section class="mb-4 inline-block w-3/4 rounded-md bg-theme-neutral-light">
+				<svelte:component this={$page.data.instructions}></svelte:component>
+			</section>
+		{/if}
+
 		{#if codeVisible}
 			<GameCode bind:value={gameDataBase64} showPlayButton showDiscardButton showCopyButton
 			></GameCode>
 		{/if}
 	</section>
 
-	<!-- Editor -->
-	<form class="mb-2 inline-block w-3/4"></form>
+	<slot />
 </main>
