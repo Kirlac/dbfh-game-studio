a5f78352971579ffe9b010898d0c056bd9e34f5b
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-23 20:58:33 +1100

    Begin implementing game editor page
    I'm going to move a lot of this into a layout so it can be shared.
    
    Committing WIP changes and additions before restructuring files.

--- src/routes/this-or-that/+page.svelte
+++ src/routes/this-or-that/+page.svelte
@@ -19,19 +19,69 @@
 		const exampleResponse = await fetch('this-or-that-example.json');
 		gameData = await exampleResponse.json();
 	}
+
+	let codeVisible = false;
+
+	function toggleCodeVisibility() {
+		codeVisible = !codeVisible;
+	}
 </script>
 
-<main class="mx-4 mt-16">
-	<!-- Editor -->
-	<form class="-ml-2">
-		<button
-			class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 m-2 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
-			on:click={loadExample}
-		>
-			<Icon name="book" class="text-theme-neutral-dark"></Icon><span class="ml-2">Load Example</span
-			>
-		</button>
-	</form>
+<header class="flex flex-col items-center justify-around text-center">
+	<img src="logo-2024-trans-200.png" alt="Desert Bus for Hope" class="inline-block w-[200px]" />
+	<h1 class="m-4 inline-block text-4xl text-theme-accent-light">This or That</h1>
+</header>
 
-	<GameCode bind:value={gameDataBase64} showPlayButton showDiscardButton showCopyButton></GameCode>
+<main class="text-center">
+	<section class="sticky top-8">
+		<menu class="mb-4 inline-flex w-3/4 items-center justify-around">
+			<li>
+				<button
+					class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
+					on:click={loadExample}
+				>
+					<Icon name="file" class="text-theme-neutral-dark"></Icon><span class="ml-2">New Game</span
+					>
+				</button>
+			</li>
+			<li>
+				<button
+					class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
+					on:click={loadExample}
+				>
+					<Icon name="book" class="text-theme-neutral-dark"></Icon><span class="ml-2"
+						>Load Example</span
+					>
+				</button>
+			</li>
+			<li>
+				<button
+					class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
+					on:click={toggleCodeVisibility}
+				>
+					<Icon name="play" class="text-theme-neutral-dark"></Icon><span class="ml-2"
+						>Play Game</span
+					>
+				</button>
+			</li>
+			<li>
+				<button
+					class="bg-stone-100 ring-stone-900/10 hover:bg-stone-200 flex items-center justify-center rounded-md p-2 text-theme-accent-dark ring-1"
+					on:click={toggleCodeVisibility}
+				>
+					<Icon name="file-code" class="text-theme-neutral-dark"></Icon><span class="ml-2"
+						>{codeVisible ? 'Hide' : 'Show'} Code</span
+					>
+				</button>
+			</li>
+		</menu>
+
+		{#if codeVisible}
+			<GameCode bind:value={gameDataBase64} showPlayButton showDiscardButton showCopyButton
+			></GameCode>
+		{/if}
+	</section>
+
+	<!-- Editor -->
+	<form class="mb-2 inline-block w-3/4"></form>
 </main>
