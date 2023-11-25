b52b1706bb6f190e1047f4cf5bdcb741c8b41f1c
Rikki Calteaux <rikkicalteaux@gmail.com>
2023-11-26 01:12:32 +1100

    Clean up styles
    Change input text to neutral dark.
    
    Adjust margins/padding.

--- src/routes/(games)/this-or-that/+page.svelte
+++ src/routes/(games)/this-or-that/+page.svelte
@@ -3,16 +3,16 @@
 </script>
 
 {#if $gameData}
-	<form class="my-6 inline-block w-3/4">
+	<form class="my-4 inline-block w-3/4">
 		<section class="grid grid-cols-1 gap-4 text-left">
-			<h2 class="text-center text-2xl text-theme-accent-light">Title Card</h2>
+			<h2 class="mt-6 text-center text-3xl text-theme-accent-light">Title Card</h2>
 			<div class="block">
 				<label for="title" class="m-2 p-2 text-theme-neutral-light">Game Title</label>
 				<input
 					id="title"
 					name="title"
 					type="text"
-					class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-accent-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
+					class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-neutral-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
 					value={$gameData.titleCard?.gameTitle}
 				/>
 			</div>
@@ -21,7 +21,7 @@
 				<textarea
 					id="description"
 					name="description"
-					class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-accent-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
+					class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-neutral-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
 					value={$gameData.titleCard?.gameDescription}
 				/>
 			</div>
@@ -32,13 +32,13 @@
 					name="title-image"
 					type="text"
 					disabled={true}
-					class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-accent-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
+					class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-neutral-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
 					value="TODO: Implement image processing"
 				/>
 			</div>
 		</section>
 		<section class="grid grid-cols-1 gap-4 text-left">
-			<h2 class="text-center text-2xl text-theme-accent-light">Questions</h2>
+			<h2 class="mt-6 text-center text-3xl text-theme-accent-light">Questions</h2>
 			{#each $gameData.questions || [{}] as question, i}
 				<div class="block">
 					<label for="question-{i + 1}-text" class="m-2 p-2 text-theme-neutral-light"
@@ -48,7 +48,7 @@
 						id="question-{i + 1}-text"
 						name="question-{i + 1}-text"
 						type="text"
-						class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-accent-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
+						class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-neutral-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
 						value={$gameData.questions?.[i].questionText}
 					/>
 				</div>
@@ -61,7 +61,7 @@
 						name="question-{i + 1}-image"
 						type="text"
 						disabled={true}
-						class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-accent-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
+						class="m-2 block w-full rounded-md bg-stone-100 p-2 text-theme-neutral-dark ring-1 ring-stone-900/10 hover:bg-stone-200"
 						value="TODO: Implement image processing"
 					/>
 					<div class="text-center">
